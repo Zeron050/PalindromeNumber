1578fb658c7a5469a864514f287816d06f9fb8de
Zeron050 <maks.surikov.05@gmail.com>
2024-12-24 20:49:21 +0500

    Add files via upload

---
+++ PalindromeNumber.java
@@ -0,0 +1,24 @@
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+
+    public static void main(String[] args) {
+        PalindromeNumber solution = new PalindromeNumber();
+
+        // Примеры
+        System.out.println("Число: 121 -> Вывод: " + solution.isPalindrome(121)); // true
+        System.out.println("Число: -121 -> Вывод: " + solution.isPalindrome(-121)); // false
+        System.out.println("Число: 10 -> Вывод: " + solution.isPalindrome(10)); // false
+    }
+}
